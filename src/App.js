e4cce83476006ca58b87bd08f566a61ca39bca18
Ibrahim <shaikhibrahim7737@gmail.com>
2021-09-22 22:40:47 +0530

    added bootstarp, added headerbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Navbar, Container, Image, Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar bg="white">
+        <Container>
+          <Navbar.Brand href="#home">
+            <Image src="https://www.paypalobjects.com/digitalassets/c/website/logo/full-text/pp_fc_hl.svg" />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link
+              href="#home"
+              onFocus={() => {
+                {
+                  <h1>Heloow</h1>;
+                }
+              }}
+            >
+              Individual
+            </Nav.Link>
+            <Nav.Link href="#features">Buiseness</Nav.Link>
+            <Nav.Link href="#pricing">Partners</Nav.Link>
+            <Nav.Link href="#pricing">Usefull Info</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
